a263bbf5e40089fa85c598db7b3c1746a51439fb
JaeHo Song <thruthesky@JaeHos-MacBook-Pro.local>
2017-02-24 19:26:02 +0800

    work on angular backend

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
+import { Http } from '@angular/http';
 export class AngularBackend {
     private urlBackend: string = "http://localhost/www/backend/index.php";
+    constructor( http: Http ) {
+        http.get( this.urlBackend ).subscribe( re => {
+            console.log( re );
+        });
+        
+    }
     getUrlBackend() {
         return this.urlBackend;
     }
